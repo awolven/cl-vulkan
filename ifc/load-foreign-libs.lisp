f3062e4319e9277f11a166922f1ee8b053cd8ff1
Andrew Wolven <awolven@gmail.com>
2020-12-17 12:28:27 -0600

    simplified load-foreign-libs.lisp

--- ifc/load-foreign-libs.lisp
+++ ifc/load-foreign-libs.lisp
@@ -21,45 +21,32 @@
 
 (in-package :cl-user)
 
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  
-(defmacro my-define-foreign-library (name-and-options &body pairs)
-  `(cffi:define-foreign-library ,name-and-options
-     ,@(mapcar (lambda (pair)
-		 (list (first pair) (eval (second pair))))
-	       pairs)))
-
 (defparameter *home-dir*
   (namestring (user-homedir-pathname)))
 
 (defparameter *cl-vulkan-dir*
   (namestring (asdf/system:system-relative-pathname :cl-vulkan "")))
 
-(defparameter *vulkan-sdk-path*
-  #+darwin (concatenate 'string *home-dir* "/vulkansdk-macos-1.1.130.0/macOS")
-  #+linux (concatenate 'string *home-dir* "/swiftshader-build1/Linux")
-  #+windows "C:/VulkanSDK/1.1.121.0"))
-
 #+darwin
-(progn
-  (sb-posix:setenv "VULKAN_SDK" *vulkan-sdk-path* 0)
-  (sb-posix:setenv "VK_ICD_FILENAMES" (concatenate 'string *vulkan-sdk-path* "/etc/vulkan/icd.d/MoltenVK_icd.json") 0)
-  (sb-posix:setenv "VK_LAYER_PATH" (concatenate 'string *vulkan-sdk-path* "/etc/vulkan/explicit_layer.d") 0)
-  (sb-posix:setenv "DYLD_LIBRARY_PATH" (concatenate 'string *vulkan-sdk-path* "/lib") 0)
-  (sb-posix:setenv "VULKAN_FRAMEWORK_PATH" (concatenate 'string *vulkan-sdk-path* "/Frameworks") 0)
-  (sb-posix:setenv "DYLD_FRAMEWORK_PATH" (concatenate 'string *vulkan-sdk-path* "/Frameworks") 0))
+(let ((vulkan-sdk-path (concatenate 'string *home-dir* "/vulkansdk-macos-1.1.130.0/macOS")))
+  
+  (sb-posix:setenv "VULKAN_SDK" vulkan-sdk-path 0)
+  (sb-posix:setenv "VK_ICD_FILENAMES" (concatenate 'string vulkan-sdk-path "/etc/vulkan/icd.d/MoltenVK_icd.json") 0)
+  (sb-posix:setenv "VK_LAYER_PATH" (concatenate 'string vulkan-sdk-path "/etc/vulkan/explicit_layer.d") 0)
+  (sb-posix:setenv "DYLD_LIBRARY_PATH" (concatenate 'string vulkan-sdk-path "/lib") 0)
+  (sb-posix:setenv "VULKAN_FRAMEWORK_PATH" (concatenate 'string vulkan-sdk-path "/Frameworks") 0)
+  (sb-posix:setenv "DYLD_FRAMEWORK_PATH" (concatenate 'string vulkan-sdk-path "/Frameworks") 0))
 
 #+linux
-(progn
-  (sb-posix:setenv "VK_ICD_FILENAMES" (concatenate 'string *vulkan-sdk-path* "/vk_swiftshader_icd.json") 0)
-  (sb-posix:setenv "VK_LAYER_PATH" (concatenate 'string *vulkan-sdk-path* "/usr/share/vulkan/explicit_layer.d") 0))
+(sb-posix:setenv "VK_ICD_FILENAMES"
+		 (concatenate 'string *home-dir* "/swiftshader-build1/Linux/vk_swiftshader_icd.json") 0)
 
-(my-define-foreign-library glfw3
+(cffi:define-foreign-library glfw3
   (:darwin "libglfw.3.4.dylib")
   (:windows "glfw3.dll")
   (:linux "libglfw.so.3"))
 
-(my-define-foreign-library vulkan-loader
+(cffi:define-foreign-library vulkan-loader
   (:linux "libvulkan.so.1")
   (:darwin "libvulkan.1.dylib")
   (:windows "vulkan-1.dll"))

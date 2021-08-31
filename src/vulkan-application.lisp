bc80fb9f9d902be50d630adb35d34be2cfa4fab5
Andrew K Wolven <awolven@gmail.com>
2021-08-31 14:00:00 -0500

    changed default title of application

--- src/vulkan-application.lisp
+++ src/vulkan-application.lisp
@@ -55,7 +55,7 @@
   (values))
 
 (defmethod initialize-instance :after ((instance vulkan-application-mixin)
-				       &rest initargs &key (title "VkTk Demo") (width 2560) (height 1440) &allow-other-keys)
+				       &rest initargs &key (title "Vulkan Demo") (width 2560) (height 1440) &allow-other-keys)
   (declare (ignore initargs))
   (setf (main-window instance)
 	(create-vulkan-window instance (default-logical-device instance) title width height))

f3b780c49903e6152288fbeb15eec14bcbaef8af
Andrew Wolven <awolven@gmail.com>
2022-11-22 12:33:47 -0600

    needed interim refactoring of memory pool

--- src/memory-pool.lisp
+++ src/memory-pool.lisp
@@ -5,33 +5,30 @@
    (lock :initform (bt:make-lock))
    (memory-allocation :reader allocation)
    (allocated :initform (make-hash-table))
-   (free :initform nil :accessor memory-pool-free)))
+   (small-free :initform nil :accessor memory-pool-small-free)
+   (medium-free :initform nil :accessor memory-pool-medium-free)
+   (large-free :initform nil :accessor memory-pool-large-free)
+   (buffer :accessor memory-pool-buffer)))
 
-(defclass vertex-buffer-memory-pool-mixin (memory-pool-mixin)
-  ((buffer :accessor memory-pool-vertex-buffer)))
+(defclass vertex-buffer-memory-pool (memory-pool-mixin) ())
 
-(defclass index-buffer-memory-pool-mixin (memory-pool-mixin)
-  ((buffer :accessor memory-pool-index-buffer)))
+(defclass index-buffer-memory-pool (memory-pool-mixin) ())
 
+(defmethod memory-pool-buffer-usage ((memory-pool vertex-buffer-memory-pool))
+  VK_BUFFER_USAGE_VERTEX_BUFFER_BIT)
 
-#+NIL(defclass small-host-visible-host-coherent-memory-pool (memory-pool-mixin)
-  ())
+(defmethod memory-pool-buffer-usage ((memory-pool index-buffer-memory-pool))
+  VK_BUFFER_USAGE_INDEX_BUFFER_BIT)
 
-#+NIL(defclass large-host-visible-host-coherent-memory-pool (memory-pool-mixin)
-  ())
-
-#+NIL(defclass large-device-local-memory-pool (memory-pool-mixin)
-  ())
-
-(defmethod initialize-instance :before ((instance vertex-buffer-memory-pool-mixin) &rest initargs
+(defmethod initialize-instance :before ((instance memory-pool-mixin) &rest initargs
                                         &key device size
                                           (properties
                                            (logior VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT
                                                    VK_MEMORY_PROPERTY_HOST_COHERENT_BIT)))
   (declare (ignore initargs))
-  (let ((test-buffer (create-buffer-1 device size VK_BUFFER_USAGE_VERTEX_BUFFER_BIT)))
+  (let ((big-buffer (create-buffer-1 device size (memory-pool-buffer-usage instance))))
     (with-vk-struct (p-requirements VkMemoryRequirements)
-      (vkGetBufferMemoryRequirements (h device) (h test-buffer) p-requirements)
+      (vkGetBufferMemoryRequirements (h device) (h big-buffer) p-requirements)
       (with-vk-struct
           (p-alloc-info VkMemoryAllocateInfo)
         (with-foreign-slots ((%vk::allocationSize
@@ -57,114 +54,101 @@
                                                                '(:struct VkMemoryRequirements)
                                                                '%vk::alignment)
                                                    :size size)))
-          (setf (allocated-memory test-buffer) (slot-value instance 'memory-allocation))
-          (bind-buffer-memory device test-buffer (slot-value instance 'memory-allocation))
-          (setf (memory-pool-vertex-buffer instance) test-buffer)))))
+          (setf (allocated-memory big-buffer) (slot-value instance 'memory-allocation))
+          (bind-buffer-memory device big-buffer (slot-value instance 'memory-allocation))
+          (setf (memory-pool-buffer instance) big-buffer)))))
   (values))
-
-(defmethod initialize-instance :before ((instance index-buffer-memory-pool-mixin) &rest initargs
-                                        &key device size
-                                          (properties
-                                           (logior VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT
-                                                   VK_MEMORY_PROPERTY_HOST_COHERENT_BIT)))
-  (declare (ignore initargs))
-  (let ((test-buffer (create-buffer-1 device size VK_BUFFER_USAGE_INDEX_BUFFER_BIT)))
-    (with-vk-struct (p-requirements VkMemoryRequirements)
-      (vkGetBufferMemoryRequirements (h device) (h test-buffer) p-requirements)
-      (with-vk-struct
-          (p-alloc-info VkMemoryAllocateInfo)
-        (with-foreign-slots ((%vk::allocationSize
-                              %vk::memoryTypeIndex)
-                             p-alloc-info
-                             (:struct VkMemoryAllocateInfo))
-          (setf %vk::allocationSize size
-                %vk::memoryTypeIndex (find-memory-type
-                                      (physical-device device)
-                                      (foreign-slot-value
-                                       p-requirements
-                                       '(:struct VkMemoryRequirements)
-                                       '%vk::memoryTypeBits)
-                                      properties))
-          (with-foreign-object (p-buffer-memory 'VkDeviceMemory)
-            (check-vk-result (vkAllocateMemory (h device) p-alloc-info (h (allocator device)) p-buffer-memory))
-            (setf (slot-value instance 'memory-allocation)
-                  (make-instance 'allocated-memory :handle (mem-aref p-buffer-memory 'VkDeviceMemory)
-                                                   :device device
-                                                   :allocator (allocator device)
-                                                   :alignment (foreign-slot-value
-                                                               p-requirements
-                                                               '(:struct VkMemoryRequirements)
-                                                               '%vk::alignment)
-                                                   :size size)))
-          (setf (allocated-memory test-buffer) (slot-value instance 'memory-allocation))
-          (bind-buffer-memory device test-buffer (slot-value instance 'memory-allocation))
-          (setf (memory-pool-index-buffer instance) test-buffer)))))
-  (values))
-
-(defun initialize-vertex-buffer-memory-pool (app)
-  (setf (vertex-buffer-memory-pool app)
-        (make-instance 'vertex-buffer-memory-pool-mixin
-                       :size (expt 2 27)
-                       :device (default-logical-device app)
-                       :properties (logior VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT
-                                           VK_MEMORY_PROPERTY_HOST_COHERENT_BIT))))
-
-(defun initialize-index-buffer-memory-pool (app)
-  (setf (index-buffer-memory-pool app)
-        (make-instance 'index-buffer-memory-pool-mixin
-                       :size (expt 2 27)
-                       :device (default-logical-device app)
-                       :properties (logior VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT
-                                           VK_MEMORY_PROPERTY_HOST_COHERENT_BIT))))
-
-#+NIL
-(defun initialize-large-host-visible-memory-pool (app)
-  (setf (large-host-visible-memory-pool app)
-        (make-instance 'large-host-visible-host-coherent-memory-pool
-                       :size (expt 2 26)
-                       :device (default-logical-device app)
-                       :properties (logior VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT
-                                           VK_MEMORY_PROPERTY_HOST_COHERENT_BIT))))
-
-#+NIL
-(defun initialize-large-device-local-memory-pool (app)
-  (setf (large-device-local-memory-pool app)
-        (make-instance 'large-device-local-memory-pool
-                       :size (expt 2 26)
-                       :device (default-logical-device app)
-                       :properties VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT)))
 
 (defstruct memory-resource
   (memory-pool)
   (offset)
   (size))
 
-(defmethod initialize-instance :after ((instance vertex-buffer-memory-pool-mixin)
+(defstruct (memory-resource-small
+	    (:include memory-resource)))
+
+(defstruct (memory-resource-medium
+	    (:include memory-resource)))
+
+(defstruct (memory-resource-large
+	    (:include memory-resource)))
+
+(defparameter *memory-pool-small-buffer-size* (expt 2 17))
+(defparameter *memory-pool-small-buffer-count* 1024)
+(defparameter *memory-pool-medium-buffer-size* (expt 2 19))
+(defparameter *memory-pool-medium-buffer-count* 512)
+(defparameter *memory-pool-large-buffer-size* (expt 2 21))
+(defparameter *memory-pool-large-buffer-count* 64)
+;; with these current numbers, for an index buffer and a vertex buffer
+;; the system tries to allocate 1G from the video card for the memory pools
+(defparameter *memory-pool-size*
+  (+ (* *memory-pool-small-buffer-count* *memory-pool-small-buffer-size*)
+     (* *memory-pool-medium-buffer-count* *memory-pool-medium-buffer-size*)
+     (* *memory-pool-large-buffer-count* *memory-pool-large-buffer-size*)))
+
+(defun initialize-vertex-buffer-memory-pool (app)
+  (setf (vertex-buffer-memory-pool app)
+        (make-instance 'vertex-buffer-memory-pool
+                       :size *memory-pool-size*
+                       :device (default-logical-device app)
+                       :properties (logior VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT
+                                           VK_MEMORY_PROPERTY_HOST_COHERENT_BIT))))
+
+(defun initialize-index-buffer-memory-pool (app)
+  (setf (index-buffer-memory-pool app)
+        (make-instance 'index-buffer-memory-pool
+                       :size *memory-pool-size*
+                       :device (default-logical-device app)
+                       :properties (logior VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT
+                                           VK_MEMORY_PROPERTY_HOST_COHERENT_BIT))))
+
+(defmethod initialize-instance :after ((instance memory-pool-mixin)
                                        &rest initargs)
   (declare (ignore initargs))
-  (loop for i from 0 below 1024
-        do (push (make-memory-resource :memory-pool instance
-                                     :offset (* i #.(expt 2 17))
-                                     :size #.(expt 2 17))
-                 (memory-pool-free instance)))
+  (loop for i from 0 below *memory-pool-small-buffer-count*
+        do (push (make-memory-resource-small
+		  :memory-pool instance
+		  :offset (* i *memory-pool-small-buffer-size*)
+		  :size *memory-pool-small-buffer-size*)
+                 (memory-pool-small-free instance)))
+  (loop for i from 0 below *memory-pool-medium-buffer-count*
+        do (push (make-memory-resource-medium
+		  :memory-pool instance
+		  :offset (+ (* i *memory-pool-medium-buffer-size*)
+			     (* *memory-pool-small-buffer-count*
+				*memory-pool-small-buffer-size*))
+		  :size *memory-pool-medium-buffer-size*)
+                 (memory-pool-medium-free instance)))
+  (loop for i from 0 below *memory-pool-large-buffer-count*
+        do (push (make-memory-resource-large
+		  :memory-pool instance
+		  :offset (+ (* i *memory-pool-large-buffer-size*)
+			     (* *memory-pool-small-buffer-count*
+				*memory-pool-small-buffer-size*)
+			     (* *memory-pool-medium-buffer-count*
+				*memory-pool-medium-buffer-size*))
+		  :size *memory-pool-large-buffer-size*)
+                 (memory-pool-large-free instance)))
   (values))
 
-(defmethod initialize-instance :after ((instance index-buffer-memory-pool-mixin)
-                                       &rest initargs)
-  (declare (ignore initargs))
-  (loop for i from 0 below 1024
-        do (push (make-memory-resource :memory-pool instance
-                                     :offset (* i #.(expt 2 17))
-                                     :size #.(expt 2 17))
-                 (memory-pool-free instance)))
-  (values))
-
-
-
-(defun %acquire-memory (memory-pool)
+(defun %acquire-memory-small (memory-pool)
   (declare (type memory-pool-mixin memory-pool))
   (bt:with-lock-held ((slot-value memory-pool 'lock))
-    (let ((free (pop (memory-pool-free memory-pool))))
+    (let ((free (pop (memory-pool-small-free memory-pool))))
+      (when free
+        (setf (gethash free (slot-value memory-pool 'allocated)) free)))))
+
+(defun %acquire-memory-medium (memory-pool)
+  (declare (type memory-pool-mixin memory-pool))
+  (bt:with-lock-held ((slot-value memory-pool 'lock))
+    (let ((free (pop (memory-pool-medium-free memory-pool))))
+      (when free
+        (setf (gethash free (slot-value memory-pool 'allocated)) free)))))
+
+(defun %acquire-memory-large (memory-pool)
+  (declare (type memory-pool-mixin memory-pool))
+  (bt:with-lock-held ((slot-value memory-pool 'lock))
+    (let ((free (pop (memory-pool-large-free memory-pool))))
       (when free
         (setf (gethash free (slot-value memory-pool 'allocated)) free)))))
 
@@ -176,57 +160,37 @@
       (if result
           (progn
             (remhash memory (slot-value memory-pool 'allocated))
-            (push memory (memory-pool-free memory-pool))
+	    (etypecase memory
+	      (memory-resource-small (push memory (memory-pool-small-free memory-pool)))
+	      (memory-resource-medium (push memory (memory-pool-medium-free memory-pool)))
+	      (memory-resource-large (push memory (memory-pool-large-free memory-pool))))
             (values))
           (error "Memory was not acquired out: ~S" memory)))))
 
+(defun %acquire-memory-sized (memory-pool size properties)
+  (declare (ignore properties))
+  (let ((free
+	  (cond ((<= size *memory-pool-small-buffer-size*)
+		 (or (%acquire-memory-small memory-pool)
+		     (%acquire-memory-medium memory-pool)
+		     (%acquire-memory-large memory-pool)))
+		((<= size *memory-pool-medium-buffer-size*)
+		 (or (%acquire-memory-medium memory-pool)
+		     (%acquire-memory-large memory-pool)))
+		((<= size *memory-pool-large-buffer-size*)
+		 (%acquire-memory-large memory-pool)))))
+    (unless free (error "could not get memory."))
+    free))
+    
 (defun acquire-vertex-memory-sized (app size properties)
-  (if (eq properties :device-local)
-      (%acquire-memory (vertex-buffer-memory-pool app))
-      (cond ((<= size #.(expt 2 17))
-             (or (%acquire-memory (vertex-buffer-memory-pool app))
-                 (error "bar")
-                 #+NIL
-                 (%acquire-memory (large-host-visible-memory-pool app))))
-            ((<= size #.(expt 2 17))
-             (error "blah")
-             #+NIL
-             (%acquire-memory (large-host-visible-memory-pool app)))
-            (t nil))))
+  (%acquire-memory-sized (vertex-buffer-memory-pool app) size properties))
 
 (defun acquire-index-memory-sized (app size properties)
-  (if (eq properties :device-local)
-      (%acquire-memory (index-buffer-memory-pool app))
-      (cond ((<= size #.(expt 2 17))
-             (or (%acquire-memory (index-buffer-memory-pool app))
-                 (error "foo")
-                 #+NIL
-                 (%acquire-memory (large-host-visible-memory-pool app))))
-            ((<= size #.(expt 2 17))
-             (error "baz")
-             #+NIL
-             (%acquire-memory (large-host-visible-memory-pool app)))
-            (t nil))))
+  (%acquire-memory-sized (index-buffer-memory-pool app) size properties))
 
-(defun vertex-release-memory (app memory)
-  (cond ((<= (memory-resource-size memory) #.(expt 2 17))
-         (%release-memory (vertex-buffer-memory-pool app) memory))
+(defun release-vertex-memory (app memory-resource)
+  (%release-memory (vertex-buffer-memory-pool app) memory-resource))
 
-        ((<= (memory-resource-size memory) #.(expt 2 17))
-         (error "yuck")
-         #+NIL
-         (if (eq (memory-resource-memory-pool memory) (large-device-local-memory-pool app))
-             (%release-memory (large-device-local-memory-pool app) memory)
-             (%release-memory (large-host-visible-memory-pool app) memory)))
-        (t (error "Cannot release ~S" memory))))
+(defun release-index-memory (app memory-resource)
+  (%release-memory (index-buffer-memory-pool app) memory-resource))
 
-(defun index-release-memory (app memory)
-  (cond ((<= (memory-resource-size memory) #.(expt 2 17))
-         (%release-memory (index-buffer-memory-pool app) memory))
-        ((<= (memory-resource-size memory) #.(expt 2 17))
-         (error "yugg")
-         #+NIL
-         (if (eq (memory-resource-memory-pool memory) (large-device-local-memory-pool app))
-             (%release-memory (large-device-local-memory-pool app) memory)
-             (%release-memory (large-host-visible-memory-pool app) memory)))
-        (t (error "Cannot release ~S" memory))))
